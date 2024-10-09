d136c7e703fdff8b9017d992783a60e92a2a414b
StudioAlbert <account@studio-albert.com>
2024-10-09 14:35:31 +0200

    solution

--- 924-iofile-Exercice/924-iofile-Exercice.cpp
+++ 924-iofile-Exercice/924-iofile-Exercice.cpp
@@ -1,19 +1,50 @@
 // 924-iofile-Exercice.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include <fstream>
 #include <iostream>
+#include <string>
+
+void LoadScore(const std::string file_name, int& score)
+{
+	// TODO : Lire le score dans le fichier
+	std::ifstream file(file_name);			
+	
+	if (file.is_open()) {
+		std::string score_string;
+
+		file >> score_string;				// récupération en string
+		score = std::stoi(score_string);	// Passage en entier
+		file.close();						// Fermeture du fichier}
+	}
+}
+
+void SaveScore(const std::string file_name, int score)
+{
+	// TODO : Ecrire le score dans le fichier
+	std::ofstream file(file_name);
+
+	if(file.is_open())
+	{
+		file << score;
+		file.close();
+	}
+}
 
 int main()
 {
+
+	const std::string file_name = ".score";
 
 	srand(time(NULL));
 
 	int score = 0;
 	bool exit = true;
 
-	// TODO : Lire le score dans le fichier
+	LoadScore(file_name, score);
 
 	do {
+
 		// On attend une touche
 		system("pause");
 
@@ -28,7 +59,7 @@ int main()
 
 	std::cout << "EXIT the clicker" << '\n';
 
-	// TODO : Ecrire le score dans le fichier
+	SaveScore(file_name, score);
 
 }
 
